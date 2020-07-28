b1187a547c2391eaf05711651eccbc84a376a21b
Damien BUTY <dam.buty@gmail.com>
2020-07-28 21:54:06 +0200

    Clean commit

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,1 @@
-import DatabaseConstructor, { Database, Statement } from 'better-sqlite3'
-
-import { Chunk } from './chunk'
-import { Cell } from './cell'
-
-export const db: Database = new DatabaseConstructor('database.db');
-
-[Chunk, Cell].forEach((model: any): void => {
-  const query: string = model.getTableSql()
-  const stmt: Statement = db.prepare(query)
-  stmt.run()
-})
+import Knex from 'knex'
